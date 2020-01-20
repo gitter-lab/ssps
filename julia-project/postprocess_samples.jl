f39bc9ad214bbaeca4f1e96133dd089e63e6bb05
david merrell <davidpmerrell@gmail.com>
2020-01-19 22:19:26 -0600

    some "changepoint" arithmetic

--- julia-project/postprocess_samples.jl
+++ julia-project/postprocess_samples.jl
@@ -11,15 +11,79 @@ module SamplePostprocess
 using JSON
 
 
-function load_samples(samples_path::String)
+"""
+Read an MCMC samples output file
+"""
+function load_samples(samples_file_path::String)
 
-    f = open(samples_path, "r")
+    f = open(samples_file_path, "r")
     str = read(f, String)
     close(f)
     d = JSON.parse(str)
 
     return d["parent_sets"], d["lambda"], d["n"]
+end
+
+
+"""
+Select the subsequence starting at `start_t` and ending at `end_t` -- *inclusive*
+"""
+function changepoint_select(changepoints::Vector, start_t::Int64, end_t::Int64)
+
+    start_idx = searchsortedfirst(changepoints, start_t; by=x->x[1])
+    end_idx = searchsortedlast(changepoints, end_t; by=x->x[1])
+
+    result = changepoints[start_idx:end_idx]
+
+    if changepoints[start_idx][1] != start_t
+        prev_v = 0
+        if start_idx > 1
+            prev_v = changepoints[start_idx - 1][2]
+        end
+        pushfirst!(result, (start_t, prev_v))
+    end
+
+    return result
+end
+
+
+
+"""
+Apply a function `op` to each entry of the sequence defined by
+the changepoint vector.
+"""
+function changepoint_map(op::Function, changepoints::Vector)
+    result = [(i, op(cp)) for cp in changepoints]
+end
+
+
+"""
+Given a vector of changepoints and a vector of endpoints,
+return the means of the sequences terminating at the endpoints.
+"""
+function sequence_means(changepoints::Vector, 
+                        endpoints::Vector{Int64})
+
+    sequence_sums = changepoint_sum(changepoints, endpoints)
     
+    start_t = changepoints[1][1]
+ 
+    return [s/(endpoints[i] - start_t + 1) for (i, s) in enumerate(sequence_sums)]
+end
+
+
+"""
+Given a vector of changepoints and a vector of endpoints,
+return the variances of the sequences terminating at the endpoints.
+"""
+function sequence_variances(changepoints::Vector,
+                            endpoints::Vector{Int64})
+    
+    means = sequence_means(changepoints, endpoints)
+    
+    for mean in means
+        
+    end
 end
 
 
@@ -27,7 +91,8 @@ end
 Given a vector of changepoints and a sorted vector of endpoints,
 return the sums of the sequences terminating at the endpoints. 
 """
-function changepoint_sum(changepoints::Vector, endpoints::Vector{Int64})
+function changepoint_sum(changepoints::Vector, 
+                         endpoints::Vector{Int64})
   
     cur_t = min(changepoints[1][1], endpoints[1])
     cur_v = 0.0
@@ -66,12 +131,6 @@ function changepoint_sum(changepoints::Vector, endpoints::Vector{Int64})
     end
     
     return sums
-end
-
-
-
-function make_lambda_means(lambda_samples::Vector; stepsize::Int64=100)
-
 end
 
 
