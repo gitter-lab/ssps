34b5f39afa736915c7188984a70d34992ccddd86
David Merrell <dmerrell@cs.wisc.edu>
2020-05-21 12:56:28 -0500

    Added a CSV comparison test for travisCI

---
+++ tests/ssps_csv.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+# A simple test that runs SSPS and makes sure it returns a 
+# reasonable-looking CSV file. It compares the SSPS output 
+# against `baseline_predictions.csv`.
+
+exit_status=0
+
+cd ../run_ssps
+snakemake --cores 2
+csvdiff --style=summary --sep=',' --output=csvdiff.out node1,node2 example_predictions.csv ../tests/baseline_predictions.csv
+
+comparison=$(cat csvdiff.out)
+rm csvdiff.out
+if [[ "$comparison" != 'files are identical' ]]; then
+    exit_status=1
+fi
+
+exit $exit_status
