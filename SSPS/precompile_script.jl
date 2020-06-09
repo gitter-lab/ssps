a65f0c77dc5b8623431384d07d6852ee27f5fc6f
David Merrell <dmerrell2@wisc.edu>
2020-06-08 20:40:12 -0500

    tinkering for TravisCI compatibility

--- SSPS/precompile_script.jl
+++ SSPS/precompile_script.jl
@@ -1,9 +1,3 @@
-using SSPS
+include("src/SSPS.jl")
 
-push!(ARGS, "../../run_ssps/example_timeseries.csv")
-push!(ARGS, "../../run_ssps/example_prior.csv")
-push!(ARGS, "dumb_output.json")
-push!(ARGS, "60")
-
-julia_main()
-
+using .SSPS
